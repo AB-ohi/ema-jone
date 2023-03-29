9d6dcdedfbf7ae3b9b4e5a2c57c9aaff878c93b0
AB-ohi <askohi63907@gmail.com>
2023-03-29 15:20:53 +0600

    lhdkjs

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+import "./Product.css";
+
+const Product = (props) => {
+  const { img, name, seller, ratings, quantity, price } = props.product;
+  const handelAddToCart = props.handelAddToCart;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <h6 className="product-name">{name}</h6>
+      <div className="product-info">
+        <p className="price">Price: ${price}</p>
+        <div className="others">
+          <p>Manufacturer: {seller}</p>
+          <p>Rating: {ratings}Star</p>
+        </div>
+      </div>
+      <button
+        onClick={() => handelAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
+    </div>
+  );
+};
+
+export default Product;
